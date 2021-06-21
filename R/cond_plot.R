80598b314e3f800459df5fbcc23e015e7483bd30
Kaijun Wang <52127961+kaijunwang19@users.noreply.github.com>
2021-06-20 17:33:15 -0700

    20210620PlotArgument

--- R/cond_plot.R
+++ R/cond_plot.R
@@ -1,4 +1,4 @@
-cond_plot<-function(UPMobj,x.target=NULL,yaxe=NULL,spline_smooth=TRUE,add=FALSE,col='red',...){
+cond_plot<-function(UPMobj,x.target=NULL,yaxe=NULL,spline_smooth=TRUE,add=FALSE,spline_args=NULL,...){
   ##function for plotting conditional density of the UPM output
   ##X.test_id: which conditional density to plot, corresponding to the order of the UPM input "X.test"
   ##UPMobj: the UPM output
@@ -12,25 +12,28 @@ cond_plot<-function(UPMobj,x.target=NULL,yaxe=NULL,spline_smooth=TRUE,add=FALSE,
   if(is.null(x.target)){
     x.target=X.test[1,]
   }
-  splineparms<-list(...)
+  plotargs<-list(...)
+  splineparms<-spline_args
   if(length(X.test_id)==0){
     stop("target X vector not found in UPM object.")
   }else{
     condfun<-UPMobj$cond.den[[X.test_id]]
     if(spline_smooth==FALSE){
-      plotx=y.axe
-      ploty=condfun(y.axe)
+      plotargs$x=y.axe
+      plotargs$y=condfun(y.axe)
     }else{
       splineparms$x=y.axe
       splineparms$y=condfun(y.axe)
       scurve<-do.call(smooth.spline,splineparms)
-      plotx=scurve$x
-      ploty=scurve$y
+      plotargs$x=scurve$x
+      plotargs$y=scurve$y
     }
+    plotargs$xlab='y'
+    plotargs$ylab=''
     if(add==FALSE){
-      plot(plotx,ploty,type="l",col=col,lwd=2, xlab="y",ylab="",main="")
+      do.call(plot,plotargs)
     }else{
-      lines(plotx,ploty,type="l",col=col,lwd=2)
+      do.call(lines,plotargs)
     }
   }
 }
